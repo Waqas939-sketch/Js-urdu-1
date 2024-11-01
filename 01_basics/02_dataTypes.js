435c6a02e2c2edcd633ca03305933345db2ce44f
Waqas939-sketch <waqasalpha89@gmail.com>
2024-11-01 16:30:00 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as newer vrsion in jawa scripit
+
+// alert (3 + 3)  // we are using node js, not browser
+
+console.log(3
+    +
+3)  // code readabillity should be high 
+
+console.log("waqas")
+    
+    
+let name = "waqas" // this is string data type
+let age = 17  // this number data type
+let isloggedIN = false/true  // this is boolen data type 
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolen => true / false 
+// null => standalone           value  mean empty
+// undefined => means           value is not assigned
+// symbol  => unique 
+
+
+// object 
+
+console.log(typeof undefined);  //outof terminal is undefined
+console.log(typeof null); //  terminal result is object 
