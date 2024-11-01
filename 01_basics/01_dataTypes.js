e22e76ced096bc88177f7d5c9103285fdf417513
Waqas939-sketch <waqasalpha89@gmail.com>
2024-11-01 15:21:37 +0000

    data types for coding

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,7 @@
+"use strict"; // tesat all js code as new version of jawa code 
+
+// alert (3 + 3) // we are using nodejs, not browser 
+
+console.log(3
++
+3) // code readability that should be high 
